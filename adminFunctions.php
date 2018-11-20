1a377ecafcc2d2040fa589940cf3ee50ebf632b1
PoMak1493 <45143412+PoMak1493@users.noreply.github.com>
2018-11-20 22:05:13 +0800

    Add files via upload
    Update Four hyper links in adminFunctions.php
    Update the SQL in uploadPaymentRecord.php

---
+++ adminFunctions.php
@@ -0,0 +1,136 @@
+<?php require('header.php');?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Sign Language Station</title>
+<link rel="stylesheet" type="text/css" href="top_bottom_list.css"/>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+	$(".dropDown").click(function(){
+    	$(".dropDownContent").slideToggle("slow");
+    });
+})
+</script>
+<style>
+.center {
+    margin: auto;
+    width: 90%;
+    padding: 5px
+}
+
+.approveVoc
+{
+    width: 100%;
+    padding: 20px;
+}
+
+.reviewReport
+{
+    width: 100%;
+    padding: 20px;
+}
+.modifyCustomer
+{
+    width: 100%;
+    padding: 20px;
+}
+.editVoc
+{
+    width: 100%;
+    padding: 20px;
+}
+
+a.approveVocLink {
+    background-color: #009999;
+    box-shadow: 0 5px #888888;
+    color: white;
+    padding: 1em 1.5em;
+    position: relative;
+    text-decoration: none;
+    text-transform: uppercase;
+}
+
+a.approveVocLink:hover {
+  background-color: #669999;
+  cursor: pointer;
+}
+
+a.approveVocLink:active {
+  box-shadow: none;
+  top: 5px;
+}
+
+a.reviewReportLink {
+    background-color: #3333cc;
+    box-shadow: 0 5px #888888;
+    color: white;
+    padding: 1em 1.5em;
+    position: relative;
+    text-decoration: none;
+    text-transform: uppercase;
+}
+
+a.reviewReportLink:hover {
+  background-color: #666699;
+  cursor: pointer;
+}
+
+a.reviewReportLink:active {
+  box-shadow: none;
+  top: 5px;
+}
+
+a.modifyCustomerLink {
+    background-color: #993366;
+    box-shadow: 0 5px #888888;
+    color: white;
+    padding: 1em 1.5em;
+    position: relative;
+    text-decoration: none;
+    text-transform: uppercase;
+}
+
+a.modifyCustomerLink:hover {
+  background-color: #cc6699;
+  cursor: pointer;
+}
+
+a.modifyCustomerLink:active {
+  box-shadow: none;
+  top: 5px;
+}
+
+a.editVocaLink {
+    background-color: #0066cc;
+    box-shadow: 0 5px #888888;
+    color: white;
+    padding: 1em 1.5em;
+    position: relative;
+    text-decoration: none;
+    text-transform: uppercase;
+}
+
+a.editVocaLink:hover {
+  background-color: #3366cc;
+  cursor: pointer;
+}
+
+a.editVocaLink:active {
+  box-shadow: none;
+  top: 5px;
+}
+</style>
+
+</head>
+<body>
+<div class="center">
+<div class ="approveVoc"><a class="approveVocLink" href=" ">Aprrove Uploaded Vocabulary</a></div>
+<div class ="reviewReport"><a class="reviewReportLink" href="" >Generate Viewing History Report</a></div>
+<div class="modifyCustomer"><a class="modifyCustomerLink" href=""> Modify Customer Information</a></div>
+<div class="editVoc"><a class="editVocaLink" href=""> Edit Vocabulary </a></div>
+</div>
+<?php require('footer.php');?>
+</body>
+</html>
--- uploadPaymentRecord.php
+++ uploadPaymentRecord.php
@@ -4,7 +4,7 @@
 <head>
 <meta charset="utf-8">
 <title>Sign Language Station</title>
-<link rel="stylesheet" type="text/css" href="css/general.css"/>
+<link rel="stylesheet" type="text/css" href="top_bottom_list.css"/>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
 $(document).ready(function(){
@@ -18,68 +18,100 @@ $(document).ready(function(){
 <?php 
 session_start();
 require_once 'connect_db.php';
+
 if(!isset($_COOKIE["username"]) && !isset($_COOKIE["email"]) )
 		header("Location: login.php");
 	
-$planID = $_SESSION['planId'];
-$price = $_SESSION['price'];
+$planID = $_POST['planId'];
+$price = $_POST['price'];
 $cardNumber = $_POST['cardnumber'];
 $nameOfCard = $_POST['nameoncard'];
 $expiryDate = $_POST['expirydate']. "-01" ;
 $secuirtyCode = $_POST['secuirtycode'];
 $cardType = $_POST['cardtype'];
-$updatePaymentQuery = "INSERT INTO payment (email, planId, paymentPrice, cardNumber, cardOwnerName, expiryDate, securityCode, cardType, paymentTime ) values (?,?,?,?,?,?,?,?, NOW());";
-if($stmt = mysqli_prepare($link, $updatePaymentQuery))
+
+$updateCreditCardQuery = "REPLACE INTO creditCardInfo (cardNumber, cardType, cardOwnerName, expiryDate , securityCode) values (?,?,?,?,?);";
+if($stmt = mysqli_prepare($link, $updateCreditCardQuery))
 {
-	mysqli_stmt_bind_param($stmt, "ssdsssss", $_COOKIE["email"], $planID, $price , $cardNumber ,  $nameOfCard , $expiryDate, $secuirtyCode, $cardType);
+	mysqli_stmt_bind_param($stmt, "isssi", $cardNumber, $cardType,  $nameOfCard , $expiryDate, $secuirtyCode);
 	
 	mysqli_stmt_execute($stmt);
 	
 	mysqli_stmt_close($stmt);
 	
-	echo "Your payment record is created. \nEnjory Learning Sign Language.";
+	
+	$updatePaymentQuery = "INSERT INTO payment (email, planId, paymentPrice, cardNumber ,paymentTime ) values (?,?,?,?, NOW());";
+	if($stmt2 = mysqli_prepare($link, $updatePaymentQuery))
+	{
+		mysqli_stmt_bind_param($stmt2, "ssds", $_COOKIE["email"], $planID, $price , $cardNumber );
+	
+		mysqli_stmt_execute($stmt2);
+	   
+		mysqli_stmt_close($stmt2);
+	
+		echo "Your payment record is created. \nEnjory Learning Sign Language.";
+	
+		
+	
+		$selectMonth = "SELECT month FROM subscriptionplan WHERE planId=? ;";
+
+		if($stmt3 = mysqli_prepare($link, $selectMonth))
+		{
+	
+			mysqli_stmt_bind_param($stmt3, 's', $planID );
+	
+			mysqli_stmt_execute($stmt3);
+	
+			mysqli_stmt_bind_result($stmt3, $month);
+	
+			while (mysqli_stmt_fetch($stmt3)) {
+				$planMonth = $month;
+			}
+	
+			mysqli_stmt_close($stmt3);
+			
+			
+			$newTimeLeft = intval($planMonth) * 31;
+			
+			$updateTimeLeftQuery = "UPDATE member SET daysLeft = daysLeft + ?  AND title = 'subscribedUser'  WHERE email =? ;";
+			if($stmt4 = mysqli_prepare($link, $updateTimeLeftQuery))
+			{
+		
+				mysqli_stmt_bind_param($stmt4, "is",$newTimeLeft, $_COOKIE["email"]);
+	
+				mysqli_stmt_execute($stmt4);
+	
+				mysqli_stmt_close($stmt4);
+	
+			}else 
+			{
+				echo "Server Error, Please connect with our administrator. ";
+			}
+				
+	
+		}else 
+		{
+			echo "Server Error, Please connect with our administrator. ";
+		}
+
+	
+	}else
+	{
+		echo "There is a server error. Please try it again.";
+	}
+
+	
 	
 }else 
 {
-	echo "The server cannot process your payment.\nPlease try it again or connect with our administrator. ";
+echo "The server cannot process your payment.\nPlease try it again or connect with our administrator. ";
 }
-$selectMonth = "SELECT month FROM subscriptionplan WHERE planId=? ;";
-if($stmt = mysqli_prepare($link, $selectMonth))
-{
-	
-	mysqli_stmt_bind_param($stmt, 's', $planID );
-	
-	mysqli_stmt_execute($stmt);
-	
-	mysqli_stmt_bind_result($stmt, $month);
-	
-	while (mysqli_stmt_fetch($stmt)) {
-        $planMonth = $month;
-    }
-	
-	mysqli_stmt_close($stmt);
-	
-}else 
-{
-	echo "Server Error, Please connect with our administrator. ";
-}
-$newTimeLeft = intval($planMonth) * 31;
-$updateTimeLeftQuery = "UPDATE member SET daysLeft = daysLeft + ? WHERE email =? ;";
-if($stmt = mysqli_prepare($link, $updateTimeLeftQuery))
-{
-	
-	mysqli_stmt_bind_param($stmt, "is",$newTimeLeft, $_COOKIE["email"]);
-	
-	mysqli_stmt_execute($stmt);
-	
-	mysqli_stmt_close($stmt);
-	
-}else 
-{
-	echo "Server Error, Please connect with our administrator. ";
-}
+
 mysqli_close($link);
 ?>
 	<?php require('footer.php');?>
 </body>
 </html>
+
+
+
