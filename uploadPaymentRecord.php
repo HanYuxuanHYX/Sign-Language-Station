d88d67ae22869afb88c754a9944f92fbcc272789
JimYYM <45063054+JimYYM@users.noreply.github.com>
2018-11-24 15:49:55 +0800

    Update uploadPaymentRecord.php

--- uploadPaymentRecord.php
+++ uploadPaymentRecord.php
@@ -3,7 +3,7 @@ session_start();
 require_once 'connect_db.php';
 if(!isset($_COOKIE["username"]) && !isset($_COOKIE["email"]) )
 		header("Location: login.php");
-	
+
 $planID = $_POST['planId'];
 $price = $_POST['price'];
 $cardNumber = $_POST['cardnumber'];
@@ -12,6 +12,7 @@ $expiryDate = $_POST['expirydate']. "-01" ;
 $secuirtyCode = $_POST['secuirtycode'];
 $cardType = $_POST['cardtype'];
 $updateCreditCardQuery = "REPLACE INTO creditCardInfo (cardNumber, cardType, cardOwnerName, expiryDate , securityCode) values (?,?,?,?,?);";
+
 if($stmt = mysqli_prepare($link, $updateCreditCardQuery))
 {
 	mysqli_stmt_bind_param($stmt, "isssi", $cardNumber, $cardType,  $nameOfCard , $expiryDate, $secuirtyCode);
@@ -49,7 +50,7 @@ if($stmt = mysqli_prepare($link, $updateCreditCardQuery))
 			
 			$newTimeLeft = intval($planMonth) * 31;
 			
-			$updateTimeLeftQuery = "UPDATE member SET daysLeft=daysLeft+? WHERE email=? AND title='subscribedUser';";
+			$updateTimeLeftQuery = "UPDATE member SET daysLeft=daysLeft+? WHERE email=? ;";
 			if($stmt4 = mysqli_prepare($link, $updateTimeLeftQuery))
 			{
 		
@@ -58,8 +59,20 @@ if($stmt = mysqli_prepare($link, $updateCreditCardQuery))
 				mysqli_stmt_execute($stmt4);
 	
 				mysqli_stmt_close($stmt4);
+
+
+				$updateTitle= "UPDATE member SET title='subscribedUser' WHERE email=? AND title='visitor';";
+				if($stmt5 = mysqli_prepare($link, $updateTitle)){
+					mysqli_stmt_bind_param($stmt5, 's', $_COOKIE["email"] );
+					mysqli_stmt_execute($stmt5);
+					mysqli_stmt_close($stmt5);
+					//if visitor pay, it becomes subscribedUser
+				}
+			
+
 				
-				echo "<script>alert('Your payment record is created. \nEnjory Learning Sign Language.')</script>";
+				
+				echo "<script>alert('Your payment record is created. Enjory Learning Sign Language.')</script>";
 	
 			}else 
 			{
@@ -83,4 +96,6 @@ if($stmt = mysqli_prepare($link, $updateCreditCardQuery))
 echo "The server cannot process your payment.\nPlease try it again or connect with our administrator. ";
 }
 mysqli_close($link);
+
+require("index.php");
 ?>
