6910d00528e5c0927d5da7a81392b09f98cbcd85
PoMak1493 <45143412+PoMak1493@users.noreply.github.com>
2018-12-02 00:20:31 +0800

    Add files via upload
    add search function : showClickingTable.php

--- showClickingTable.php
+++ showClickingTable.php
@@ -20,37 +20,38 @@
       <script type = "text/javascript">
          google.charts.load('current', {packages: ['table']});     
 		 google.charts.load('current', {packages: ['corechart','line']});
+		 google.charts.load('current', {'packages':['corechart', 'controls']});
       </script>
       </script>
    </head>
    
    <body>
-      <div id = "container" style = "width: 550px; height: 400px; margin: 60 auto">
-      </div>
+      <div id="dashboard_div"  style = "width: 550px; height: 400px; margin: 60 auto">
+			<div id='vocab_filter'  ></div>
+			<div id = "vocab_table" ></div>
+	  </div>
       <script language = "JavaScript">
          function drawChart() {
-            var data = new google.visualization.DataTable();
-            data.addColumn('string', 'Vocabulary ID ');
-            data.addColumn('string', 'Vocabulary Name');
-            data.addColumn('number', 'Check Total');
-            data.addRows([
+			 
+			 var data = google.visualization.arrayToDataTable([
+					['Vocabulary ID', 'Vocabulary Name' , 'Check Total'],
+			
 			<?php
 					session_start();
 					require_once 'connect_db.php';
 					if(!isset($_COOKIE["username"]) && !isset($_COOKIE["email"]) )
 							header("Location: login.php");
 							
-						$sql1 = "SELECT * FROM permission 
-			WHERE title='" . $_COOKIE["title"] . "'";
-	$result1 = mysqli_query($link,$sql1) or die("SQL error!<br>");
-	$row1 = mysqli_fetch_array($result1, MYSQLI_ASSOC);
+					$sql1 = "SELECT * FROM permission WHERE title='" . $_COOKIE["title"] . "'";
+					$result1 = mysqli_query($link,$sql1) or die("SQL error!<br>");
+					$row1 = mysqli_fetch_array($result1, MYSQLI_ASSOC);
 	
-	if($row1["statistics"]==0){
-		mysqli_close($link);
-		echo "<script>alert('You do not have the authority to do this!');
-		window.location.href='adminFunctions.php';</script>";
-		exit;
-	}
+					if($row1["statistics"]==0){
+								mysqli_close($link);
+								echo "<script>alert('You do not have the authority to do this!');
+								window.location.href='adminFunctions.php';</script>";
+								exit;
+					}
 	
 					$findVocab = "SELECT vocabId, vocabName, checkTotal FROM vocabulary;";
 					
@@ -71,19 +72,39 @@
 						echo "Cannot obtain the vocabulary information. Please connect to the IT department.";
 					}
                 ?>]);
-            var options = {
-               showRowNumber: true,
-               width: '100%', 
-               height: '100%',
-               pageSize: 5
-            };
-			
-			var chart = new google.visualization.Table(document.getElementById('container'));
-			
+				
+			var dashboard = new google.visualization.Dashboard(document.getElementById('dashboard_div'));
+			 
+			var stringFilter = new google.visualization.ControlWrapper({
+						'controlType': 'StringFilter',
+						'containerId': 'vocab_filter',
+						'options': {
+							'matchType': 'prefix',
+							'filterColumnIndex': '1',
+							'ui':
+							{
+								'label': 'Search Vocabulary: ',
+							}
+							}
+				});
+				
+			var vocabTable = new google.visualization.ChartWrapper({
+						'chartType': 'Table',
+						'containerId': 'vocab_table',
+						'options': {
+								'width': '100%',
+								'height': '100%',
+								'pageSize': '5',
+								'showRowNumber': 'true',
+				}
+			});
+
+			 dashboard.bind(stringFilter, vocabTable);
+						
 			function selectHandler() {
-			var selectedItem = chart.getSelection()[0];
+			var selectedItem = vocabTable.getChart().getSelection()[0];
 			if (selectedItem) {
-				var vocabId = data.getValue(selectedItem.row,0);
+				var vocabId = vocabTable.getDataTable().getFormattedValue(selectedItem.row,0);
 				$(function(){
 				$.ajax({
 						type: "POST",
@@ -101,8 +122,8 @@
 			}
 			
 			
-            google.visualization.events.addListener(chart, 'select', selectHandler);       
-            chart.draw(data, options);
+            google.visualization.events.addListener(vocabTable, 'select', selectHandler);       
+            dashboard.draw(data);
          
 		 
 			  }
